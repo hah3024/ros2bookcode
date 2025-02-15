221184bb1f4ade5aba35a425f080e7878b4bb769
hah3024 <1523311435@qq.com>
2025-02-15 18:05:20 +0000

    modify the writer_node

--- chapt2/chapt2_ws/src/demo_python_pkg/demo_python_pkg/writer_node.py
+++ chapt2/chapt2_ws/src/demo_python_pkg/demo_python_pkg/writer_node.py
@@ -1,11 +1,14 @@
 from demo_python_pkg.person_node import PersonNode
-
+import rclpy
 class WriterNode(PersonNode):
     def __init__(self, name: str, age: int, book: str) -> None:
-        super().__init__(name, age)
+        super().__init__('writer_node',name, age)
         print('WriterNode 的 __init__ 函数被调用了')
         self.book = book
 
 def main():
-    node = WriterNode('法外狂徒张三', 18, '论快速入狱')
+    rclpy.init()
+    node = WriterNode('法外狂徒张三', age=18, book ='论快速入狱')
     node.eat('鱼香肉丝')
+    rclpy.spin(node)
+    rclpy.shutdown()
